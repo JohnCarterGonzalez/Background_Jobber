21bbdf762a38cca44ae0dc5233df1a7e1081dd8b
PascalBana <pascalrey1955@gmail.com>
2023-07-15 13:59:06 -0600

    undid changes to worker class

--- lib/worker.rb
+++ lib/worker.rb
@@ -21,12 +21,10 @@ module BackgroundJobber
         current_serialized_job = @cache.pop(queue_name)
 
         break if current_serialized_job.nil?
-          # rewrote this section of code to make it slightly more readable and testable.
-          # I also added a check to see if the job is an array, if it is, it passes the args
-          # as an array, if not, it passes the args as a single arg
-        class_name, args = deserialize_job(job)
-        job_class = Object.const_get(class_name)
-        job_args = args.is_a?(Array) ? args : [args]  
+        current_job_components = deserialize_job(current_serialized_job)
+
+        job_class = current_job_components.first
+        job_args = current_job_components.last  
         
         job_class.new.perform(*job_args)
       end
